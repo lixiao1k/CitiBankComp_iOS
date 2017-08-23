b767f5ba31e6e98da8fa4c36a420494421989f44
李晓冬 <lixiao1k@163.com>
2017-08-23 20:15:47 +0800

    实现了多个textfield的pickerview输入

--- CitiBank/CitiBank/InvestPlanViewController.swift
+++ CitiBank/CitiBank/InvestPlanViewController.swift
@@ -8,30 +8,36 @@
 
 import UIKit
 
-class InvestPlanViewController: UIViewController {
+class InvestPlanViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource {
 
     @IBOutlet weak var invest_amount_tf: Picker_TextField!
     
     @IBOutlet weak var invest_time_tf: Picker_TextField!
     
-    @IBOutlet weak var amount_pv: UIPickerView!
+    var amount_data = [1,2,3,4,5,6,7,8,9]
+    var time_data = [1,3,6,9,12]
     
-    @IBOutlet weak var time_pv: UIPickerView!
-    
-    var test: UIPickerView!
-    
+//    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        let amount_pv = UIPickerView()
+        amount_pv.delegate = self
+        amount_pv.dataSource = self
+        amount_pv.tag = 1
+        invest_amount_tf.inputView = amount_pv
+        
+        let time_pv = UIPickerView()
+        time_pv.delegate = self
+        time_pv.dataSource = self
+        time_pv.tag = 2
+        invest_time_tf.inputView = time_pv
+        
+        //点击空白处隐藏pickerview
+        let tap = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard(tapG:)))
+        tap.cancelsTouchesInView = false
+        self.view.addGestureRecognizer(tap)
         // Do any additional setup after loading the view.
-        
-        invest_amount_tf.addTarget(self, action: #selector(print1), for:UIControlEvents.editingDidBegin)
-        
-        test = UIPickerView()
-        invest_amount_tf.inputView = test
-        
-    }
-    func print1()  {
         
     }
 
@@ -39,7 +45,42 @@ class InvestPlanViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
     
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView.tag == 1 {
+            return amount_data.count
+        }
+        if pickerView.tag == 2 {
+            return time_data.count
+        }
+        return 0
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if pickerView.tag == 1 {
+            return "\(amount_data[row])"
+        }
+        if pickerView.tag == 2 {
+            return "\(time_data[row])"
+        }
+        return nil
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if pickerView.tag == 1 {
+            invest_amount_tf.text = "\(amount_data[row])"
+        }
+        if pickerView.tag == 2 {
+            invest_time_tf.text = "\(time_data[row])"
+        }
+    }
+    
+    func hideKeyboard(tapG:UITapGestureRecognizer)  {
+        self.view.endEditing(true)
+    }
 
     /*
     // MARK: - Navigation
