40a77c10826cfc2421a3bbe39a4b1e98f597afd8
李晓冬 <lixiao1k@163.com>
2017-09-28 00:46:38 +0800

    文档

--- CitiBank/CitiBank/Loan_Scatted_ViewController.swift
+++ CitiBank/CitiBank/Loan_Scatted_ViewController.swift
@@ -7,13 +7,44 @@
 //
 
 import UIKit
+import M13Checkbox
+class Loan_Scatted_ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource {
 
-class Loan_Scatted_ViewController: UIViewController {
+    @IBOutlet weak var loan_amount_tf: Picker_TextField!
 
+    
+    @IBOutlet weak var loan_duration_tf: Picker_TextField!
+    
+    @IBOutlet weak var interest_tf: Picker_TextField!
+    
+    @IBOutlet weak var agree: M13Checkbox!
+    
+    
+    
+    @IBAction func loan(_ sender: UIButton) {
+    }
+    
+    
+    var isAgreed: Bool = false
+    var amount_data = [1,2,3,4,5,6]
+    var time_data = [1,3,6,9,12]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let amount_pv = UIPickerView()
+        amount_pv.delegate = self
+        amount_pv.dataSource = self
+        amount_pv.tag = 1
+        loan_amount_tf.inputView = amount_pv
+        
+        let time_pv = UIPickerView()
+        time_pv.delegate = self
+        time_pv.dataSource = self
+        time_pv.tag = 2
+        loan_duration_tf.inputView = time_pv
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,7 +52,40 @@ class Loan_Scatted_ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView.tag == 1{
+            return amount_data.count
+        }
+        if pickerView.tag == 2{
+            return time_data.count
+        }
+        return 0
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if pickerView.tag == 1 {
+            return "\(amount_data[row])"
+        }
+        if pickerView.tag == 2 {
+            return "\(time_data[row])"
+        }
+        return nil
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if pickerView.tag == 1 {
+            loan_amount_tf.text = "\(amount_data[row])千元"
+        }
+        if pickerView.tag == 2 {
+            loan_duration_tf.text = "\(time_data[row])个月"
+        }
+    }
+    
+    
     /*
     // MARK: - Navigation
 
